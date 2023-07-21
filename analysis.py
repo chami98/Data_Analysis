6e1cbcaa5c245fb61856e96e37022f0461e77ee9
Chamikara Mendis <chamikaram@99x.io>
2023-07-21 14:30:39 +0530

    Return TecDocNo array

--- analysis.py
+++ analysis.py
@@ -4,30 +4,36 @@ import json
 # Replace 'data.csv' with the actual path to your CSV file.
 csv_file = 'dataset.csv'
 
-result = []
+result = {}
 seen_combinations = set()
 
 with open(csv_file, 'r') as file:
     csv_reader = csv.DictReader(file, delimiter=';')
     for row in csv_reader:
-        combination = (
-            row['VehicleMake'],
-            row['Model'],
-            row['ModelYear'],
-            row['TecDocNo'],
-        )
-        if combination not in seen_combinations:
-            row_dict = {
-                'VehicleMake': row['VehicleMake'],
-                'Model': row['Model'],
-                'ModelYear': row['ModelYear'],
-                'TecDocNo': row['TecDocNo'],
+        # Strip unwanted spaces from the fields.
+        vehicle_make = row['VehicleMake'].strip()
+        model = row['Model'].strip()
+        model_year = row['ModelYear'].strip()
+        tec_doc_no = row['TecDocNo'].strip()
+
+        combination = (vehicle_make, model, model_year)
+        if combination in seen_combinations:
+            if tec_doc_no not in result[combination]['TecDocNo']:
+                result[combination]['TecDocNo'].append(tec_doc_no)
+        else:
+            result[combination] = {
+                'VehicleMake': vehicle_make,
+                'Model': model,
+                'ModelYear': model_year,
+                'TecDocNo': [tec_doc_no],  # Initialize TecDocNo as a list with the first value.
             }
-            result.append(row_dict)
             seen_combinations.add(combination)
 
-# Sort the 'result' list based on the 'VehicleMake' key in alphabetical order.
-result_sorted = sorted(result, key=lambda x: x['VehicleMake'])
+# Convert the 'result' dictionary values to a list of dictionaries.
+result_sorted = list(result.values())
+
+# Sort the 'result_sorted' list based on the 'VehicleMake' key in alphabetical order.
+result_sorted.sort(key=lambda x: x['VehicleMake'])
 
 # Replace 'output.json' with the desired file path and name for the JSON file.
 output_file = 'output.json'
